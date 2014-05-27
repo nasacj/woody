6d812d854d9f8bfd58f906d544f5d5bb3eda8005
nasadiscovery <cj.nasa@gmail.com>
2014-05-27 10:21:25 +0800

    Initial Java version Eclipse Project

---
+++ src/xmpp/nasacj/learn/HelloMXParser.java
@@ -0,0 +1,15 @@
+package xmpp.nasacj.learn;
+
+public class HelloMXParser
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
